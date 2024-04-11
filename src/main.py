485b0a1381b431557a0b344196e432b9c5a8906d
Matthew Thomas <adriant172@gmail.com>
2024-04-11 23:35:13 +0900

    add copy_static files functionality and add to main function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from textnode import TextNode
 from inline_markdown import split_nodes_delimiter, split_nodes_image, split_nodes_links, text_to_textnodes
 from htmlnode import HTMLNode
+from copy_static import copy_dir, clear_dir
+
+static_path = "./static"
+public_path = "./public"
 
 def main():
-    test_text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
-    result = text_to_textnodes(test_text)
-    print(result)
-
+    clear_dir(public_path)
+    copy_dir(static_path, public_path)
 
 main()
